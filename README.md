fee0d4e5141f7138ffb911f7c1493ba34a9808cc
Jeronimo-lab <jeroanime@gmail.com>
2025-07-03 23:54:58 -0300

    VariasCorrecciones

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# tp_plp_2
-El segundo tp de plp
+Correcciones: 
+1- CUANDO ENTREGUEMOS HAGAMOS ZIP EN UNA CARPETA QUE TENGA KATAMINO.PL Y PIEZAS.PL... NADA MAS
+2- WARNINGS MULTIPLES POR TODOS LADOS. (Corregido)
+3- CONVENCION DE NOMBRES PARA PREDICADOS NO CONSISTENTE (Corregido)
+4- MEMBERAUX NUNCA SE USA (Borrado, Corregido)
+5- Tienen predicados k-piezas-para-consultar/2 y seccionTablero/5 con definición comentados en su código. No entreguen fragmentos de código deprecado. (Corregido, menos seccionTablero)
+6- FALTAN EJERCICIOS 11 Y 12
+7- EJERCICIO 2: LENGTH_AUX NO ES UN NOMBRE DESCRIPTIVO (Corregido)
+8- EJERCICIO 3: EL USO DE LAST ES REDUNDANTE, PODEMOS USAR UNIFICACION NATIVA DE PROLOG PARA RELACIONAR EN UN LENGTH LA LONGITUD DE LAS FILAS Y LAS COLUMNAS (Corregido)
+9- EJERCICIO 5: EL PREDICADO A DEFINIR ES kPiezas/2 NO k-piezas/2 (Corregido)
+10- EJERCICIO 6: SUBLISTA YA OFRECE EL TABLERO SECCIONADO CON EL CUAL TRABAJAR, NO ES NECESARIO RELACIONAR LOS ARGUMENTOS CON TABLERO. EL PREDICADO NO TIENE EL COMPORTAMIENTO ESPERADO. (Corregido)
+11- EJERCICIO 7: El predicado no tiene el comportamiento esperado. Por ejemplo, faltan rotaciones de una misma pieza.
+12- EJERCICIO 8: El predicado no tiene el comportamiento esperado. Depende del ejercicio anterior.
+13- EJERCICIO 9: El predicado no tiene el comportamiento esperado. Depende del ejercicio anterior.
+14- EJERCICIO 10: Dado que los ejercicios previos no están resueltos adecuadamente, se requiere volver a computar las métricas para la re-entrega.
+15- EJERCICIO 11 Y 12 SIN HACER. 
--- katamino.pl
+++ katamino.pl
@@ -4,20 +4,22 @@
 sublista(Descartar, Tomar, Lista, Res) :- 
     append(Descarte1, Resto, Lista),
     length(Descarte1, Descartar), 
-    append(Res, Descarte2, Resto),
+    append(Res, _, Resto),
     length(Res, Tomar).
 
-%!length_aux(?K,?L).
-    length_aux(K,L) :- length(L,K).
+%!lengthCompatiblesConMapList(?K,?L).
+lengthCompatiblesConMapList(K,L) :- 
+    length(L,K).
 
 %!tablero(+K,-T)
-    tablero(K,T) :- length(T,5), maplist(length_aux(K),T).
+tablero(K,T) :- 
+    length(T,5), 
+    maplist(lengthCompatiblesConMapList(K),T). %Aca hice correccion 7 (ver en readme)
 
 %!tamanio(+M,-F,-C)
-tamanio(Matriz, CantFilas, CantColumnas) :- 
-    last(Matriz, UltimaFila),           % Instanciamos una fila de la matriz
-    length(Matriz, CantFilas),          % Sacamos la longitud de la matriz (Cant Filas)
-    length(UltimaFila, CantColumnas).   % Sacamos la longitud de la fila (Cant Columnas)
+tamanio([Fila|Matriz], CantFilas, CantColumnas) :- 
+    length([Fila|Matriz], CantFilas),    % Sacamos la longitud de la matriz (Cant Filas)
+    length(Fila, CantColumnas).   % Sacamos la longitud de la fila (Cant Columnas)
 
 %!coordenadas(+T,-IJ)
 coordenadas(Matriz, (Fil,Col)) :- 
@@ -25,49 +27,32 @@ coordenadas(Matriz, (Fil,Col)) :-
     between(1, CantFila, Fil), 
     between(1, CantCol, Col).
           
-%!memberAux(+P, ?Pieza)
-memberAux(P, Pieza) :- member(Pieza, P).
-
 %!estaOrdenado(?Lista, ?Res).
 estaOrdenado(_,[]).
 estaOrdenado([X|PS],[X|XS]) :- estaOrdenado(PS,XS).
 estaOrdenado([_|PS],XS) :- estaOrdenado(PS,XS).
 
-%k-piezas-para-consultar(+K, -PS)
-%k-piezas-para-consultar(K, PS) :- 
-    %nombrePiezas(P), 
-    %length(PS,K), 
-    %estaOrdenado(P,PS).
-
-k-piezas(K, Res) :- 
+kPiezas(K, Res) :- 
     nombrePiezas(Piezas),
      setof(SolucionFiltrada, (estaOrdenado(Piezas, SolucionFiltrada), length(SolucionFiltrada, K)), ListaFiltrada), 
      member(Res, ListaFiltrada).
 
 %!seccionTablero(+T,+ALTO, +ANCHO, +IJ, ?ST)
-seccionTablero(_, 0, _, _, []). %Caso Base
-
-%seccionTablero(Tablero, Alto, Ancho, (I,J), [X|XS]) :- 
-%    nth1(I, Tablero, Fila), %Instancio Fila con una fila de Tablero, para calcular el ancho de la matriz.
-%    length(Fila, Anchotablero), Descartar is Anchotablero - Ancho, %Instaciamos lo que vamos a descartar en la fila
-%    sublista(Descartar, Ancho, Fila, X), %Instanciamos X como la seccion de la matriz que queremos buscar
-%    I2 is I+1, %Avanzamos a la siguiente fila
-%    Alto2 is Alto-1, %Resto al alto para hacer recursion
-%    seccionTablero(Tablero, Alto2, Ancho, (I2,J), XS). %Recursion
-
 seccionTablero(Tablero, Alto, Ancho, (I,J), XS) :-
-    tablero(Ancho, XS2),
-    tamanio(Tablero, AltoTotal, AnchoTotal),
-    DescarteAncho is AnchoTotal - Ancho,
-    maplist(sublista(DescarteAncho, Ancho), Tablero, XS2),
-    DescarteAlto is AltoTotal - Alto,
-    sublista(DescarteAlto,Alto,XS2,XS).
-    
-%No llegamos con el tiempo, porque pensamos que se entregaba el viernes 20/06 a las 3:59. 
+    DescarteAncho is J-1,
+    DescarteAlto is I-1,
+    maplist(sublista(DescarteAncho, Ancho), Tablero, TableroIntermedio),
+    sublista(DescarteAlto,Alto,TableroIntermedio,XS).
 
 
-%!ubicarPieza(+Tablero, +Identificador).
-ubicarPieza(Tablero, Identificador) :- pieza(Identificador, ID), tamanio(ID, Fila, Colum), seccionTablero(Tablero, Fila, Colum, IJ, ID).
+%!ubicarPieza(+Tablero, +Identificador). 
+ubicarPieza(Tablero, Identificador) :- 
+    pieza(Identificador, ID), 
+    tamanio(ID, AltoPieza, AnchoPieza),
+    tamanio(Tablero, AltoTablero, AnchoTablero),
+    between(1,AltoTablero,I),
+    between(1,AnchoTablero,J),
+    seccionTablero(Tablero, AltoPieza, AnchoPieza, (I, J), ID).
 
 %!poda(+Poda, +Tablero)
 poda(sinPoda, _). 
@@ -84,7 +69,7 @@ ubicarPiezas(Tablero, Poda, Identificadores) :-
 %!llenarTablero(+Poda, +Columnas,-Tablero)
 llenarTablero(Poda, Columnas, Tablero) :- 
     tablero(Columnas, Tablero), 
-    k-piezas(Columnas, IDList), 
+    kPiezas(Columnas, IDList), 
     ubicarPiezas(Tablero, Poda, IDList).
 
 
